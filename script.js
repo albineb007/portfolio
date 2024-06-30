0de08da9362700a71f838cbfff408a184e34ff1e
Albin EB <74339427+albineb007@users.noreply.github.com>
2024-06-30 18:57:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-  let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// Get the menu icon element and the navbar element
+const menu = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
 
+// Toggle menu and navbar classes on menu click
 menu.onclick = () => {
   menu.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 };
 
+// Remove menu and navbar classes on window scroll
 window.onscroll = () => {
   menu.classList.remove('bx-x');
   navbar.classList.remove('active');
 };
 
+// Initialize ScrollReveal with configuration
 const sr = ScrollReveal({
-  distance: '60px',
-  duration: 2500,
-  reset: true,
+  distance: '60px',  // Movement distance of elements
+  duration: 2500,    // Animation duration in milliseconds
+  reset: true        // Enable resetting animations
 });
 
+// Reveal elements with specific configurations
 sr.reveal('.home-text', { delay: 200, origin: 'top' });
 sr.reveal('.home-img', { delay: 400, origin: 'top' });
 sr.reveal('.about, .cta, .resume, .contact, .footer', { delay: 200, origin: 'top' });
